0f603c67e4611e09db544762cd453711a09ae3c8
Josh <jshbker@gmail.com>
2023-10-15 17:21:31 +0100

    Code cleanup changes, try custom red prefix

--- src/main/kotlin/com/learnspigot/event/util/npc/NPCListener.kt
+++ src/main/kotlin/com/learnspigot/event/util/npc/NPCListener.kt
@@ -7,7 +7,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.world.ChunkLoadEvent
 
-class NPCListener : Listener {
+object NPCListener : Listener {
 
     @EventHandler
     fun onPlayerInteractEntity(e: PlayerInteractEntityEvent) {
