4a589bdd1ab5309a3b624064fb0cb44719689ebe
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2023-11-04 02:33:27 +0000

    NoteblockAPI integration

--- src/main/kotlin/com/learnspigot/event/engine/game/MusicalMinecartsGame.kt
+++ src/main/kotlin/com/learnspigot/event/engine/game/MusicalMinecartsGame.kt
@@ -1,11 +1,33 @@
 package com.learnspigot.event.engine.game
 
 import com.learnspigot.event.engine.Game
+import com.learnspigot.event.util.NBSSongType
+import com.xxmicloxx.NoteBlockAPI.model.RepeatMode
+import com.xxmicloxx.NoteBlockAPI.songplayer.RadioSongPlayer
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 class MusicalMinecartsGame : Game() {
 
+    private lateinit var music: RadioSongPlayer
+
     override fun start() {
+        val song = NBSSongType.entries.random()
+        music = RadioSongPlayer(song.song)
+        music.repeatMode = RepeatMode.ALL
+
+        Bukkit.getOnlinePlayers().forEach(music::addPlayer)
+
+        Bukkit.broadcast(Component.text("now playing ${song.title}"))
+
+
+        newRound()
+    }
+
+    private fun newRound() {
+        music.isPlaying = true
+
 
     }
 
