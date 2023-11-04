bf0ab48b0eb3c2d2b8716edc669f1a1c9068e7ac
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2023-11-04 23:31:00 +0000

    IM CHRIS HANSEN FROM DATELINE NBC

--- src/main/kotlin/com/learnspigot/event/engine/game/MusicalMinecartsGame.kt
+++ src/main/kotlin/com/learnspigot/event/engine/game/MusicalMinecartsGame.kt
@@ -1,19 +1,54 @@
 package com.learnspigot.event.engine.game
 
+import com.learnspigot.event.ChristmasEvent
 import com.learnspigot.event.engine.Game
+import com.learnspigot.event.engine.GameEngine
+import com.learnspigot.event.engine.GameType
+import com.learnspigot.event.util.MapLocation
 import com.learnspigot.event.util.NBSSongType
 import com.xxmicloxx.NoteBlockAPI.model.RepeatMode
 import com.xxmicloxx.NoteBlockAPI.songplayer.RadioSongPlayer
+import gg.flyte.twilight.event.event
+import gg.flyte.twilight.extension.*
+import gg.flyte.twilight.scheduler.delay
+import gg.flyte.twilight.scheduler.repeat
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
+import org.bukkit.Sound
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Minecart
 import org.bukkit.entity.Player
+import org.bukkit.event.vehicle.VehicleEnterEvent
+import org.bukkit.scheduler.BukkitTask
+import org.bukkit.util.BoundingBox
+import kotlin.math.roundToInt
+import kotlin.random.Random
 
 class MusicalMinecartsGame : Game() {
 
+    private val MINECART_SPAWN = BoundingBox.of(
+        MapLocation(-147, 86, 71),
+        MapLocation(-133, 86, 58)
+    )
+
     private lateinit var music: RadioSongPlayer
 
-    override fun events() {
+    private var alive = mutableListOf<Player>()
+    private var inMinecart = mutableListOf<Player>()
+    private var minecarts = mutableListOf<Minecart>()
 
+    private var ticker: BukkitTask? = null
+
+    override fun events() {
+        events += event<VehicleEnterEvent> {
+            if (vehicle.type == EntityType.MINECART && entered.type == EntityType.PLAYER) {
+                inMinecart += entered as Player
+                if (inMinecart.size == minecarts.size) {
+                    newRound()
+                }
+            }
+        }
     }
 
     override fun start() {
@@ -25,7 +60,7 @@ class MusicalMinecartsGame : Game() {
 
         Bukkit.broadcast(Component.text("now playing ${song.title}"))
 
-
+        alive.addAll(Bukkit.getOnlinePlayers())
 
         newRound()
     }
@@ -33,11 +68,78 @@ class MusicalMinecartsGame : Game() {
     private fun newRound() {
         music.isPlaying = true
 
+        inMinecart.clear()
 
+        tasks += delay((Random.nextInt(5) + 10) * 20) {
+            music.isPlaying = false
+
+            Bukkit.getOnlinePlayers().applyForEach { playSound(Sound.BLOCK_NOTE_BLOCK_BASEDRUM) }
+
+            // SPAWNING MINECARTS
+            val potentialSpawnLocations = MINECART_SPAWN.getLocations(ChristmasEvent.WORLD)
+            kotlin.repeat((alive.size * 0.66).roundToInt()) {
+                minecarts += potentialSpawnLocations.random().spawnEntity(EntityType.MINECART) as Minecart
+            }
+
+            // TICKER
+            var iter = 40
+            tasks += repeat(5, 5) {
+                if (iter == 0) {
+                    endRound()
+                    return@repeat
+                }
+
+                val component = Component.text()
+                for (i in 1 until 41) {
+                    component.append(Component.text('|').color(if (i >= iter) NamedTextColor.GRAY else NamedTextColor.GREEN))
+                }
+
+                Bukkit.getOnlinePlayers().applyForEach {
+                    sendActionBar(component)
+                    playSound(Sound.BLOCK_STONE_BUTTON_CLICK_ON)
+                }
+
+                iter--
+            }.also { ticker = it }
+        }
+    }
+
+    private fun endRound() { // When either all Minecarts are filled, or 10 second timer over
+        ticker?.cancel()
+        Bukkit.getOnlinePlayers().applyForEach { clearActionBar() }
+
+        // ADD SCORES AND REMOVE PLAYERS NOT IN MINECART
+        val iterator = alive.iterator()
+        while (iterator.hasNext()) {
+            val player = iterator.next()
+            if (player.isInsideVehicle && player.vehicle!!.type == EntityType.MINECART) {
+                points[player.uniqueId] = points.getOrDefault(player.uniqueId, 0) + 1
+            } else {
+                player.teleport(GameType.MUSICAL_MINECARTS.spectatorSpawn!!)
+                player.playSound(Sound.ENTITY_PLAYER_DEATH)
+                Bukkit.getOnlinePlayers().applyForEach { sendMessage("$${player.name} was eliminateddd") }
+                iterator.remove()
+            }
+        }
+
+        // CLEAR MINECARTS
+        minecarts.applyForEach {
+            remove()
+        }
+
+        // CHECK FOR WINNER
+        if (alive.size <= 1) {
+            music.destroy()
+            GameEngine.stop()
+        } else {
+            newRound()
+        }
     }
 
     override fun stop() {
-
+        minecarts.applyForEach {
+            remove()
+        }
     }
 
     override fun onPlayerJoin(player: Player) {
--- src/main/kotlin/com/learnspigot/event/engine/game/SledRacingGame.kt
+++ src/main/kotlin/com/learnspigot/event/engine/game/SledRacingGame.kt
@@ -3,15 +3,12 @@ package com.learnspigot.event.engine.game
 import com.learnspigot.event.ChristmasEvent
 import com.learnspigot.event.engine.Game
 import com.learnspigot.event.engine.GameEngine
-import com.learnspigot.event.engine.GameState
 import com.learnspigot.event.util.MapLocation
 import gg.flyte.twilight.event.event
 import gg.flyte.twilight.extension.applyForEach
 import gg.flyte.twilight.extension.contains
 import gg.flyte.twilight.extension.getBlocks
-import gg.flyte.twilight.extension.getLocations
 import gg.flyte.twilight.scheduler.repeat
-import gg.flyte.twilight.time.TimeUnit
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.Material
@@ -19,7 +16,6 @@ import org.bukkit.entity.Boat
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.event.vehicle.VehicleExitEvent
 import org.bukkit.util.BoundingBox
 
@@ -41,9 +37,7 @@ class SledRacingGame : Game() {
     private val boats = mutableMapOf<Player, Boat>()
 
     init {
-        STARTING_WALL.getBlocks(ChristmasEvent.WORLD).forEach {
-            it.type = Material.OAK_FENCE
-        }
+        STARTING_WALL.getBlocks(ChristmasEvent.WORLD).applyForEach { type = Material.OAK_FENCE }
 
         Bukkit.getOnlinePlayers().forEach { player ->
             (player.world.spawnEntity(player.location, EntityType.BOAT) as Boat).apply {
@@ -52,8 +46,6 @@ class SledRacingGame : Game() {
                 boats[player] = this
             }
         }
-
-
     }
 
     override fun events() {
@@ -64,42 +56,38 @@ class SledRacingGame : Game() {
         events += event<EntityDamageEvent> {
             if (entityType == EntityType.BOAT) isCancelled = true
         }
-
-        events += event<PlayerMoveEvent> { // Won't lag, very simple maths check
-            if (state == GameState.ACTIVE && FINISHING_AREA.contains(player.location) && !points.containsKey(player.uniqueId)) {
-                points[player.uniqueId] = -1
-                player.sendMessage("you finished!!! in ${System.currentTimeMillis() - startTime!!}")
-
-                val barrierBlockData = Material.BARRIER.createBlockData()
-                STARTING_WALL.getLocations(ChristmasEvent.WORLD).forEach {
-                    player.sendBlockChange(it, barrierBlockData)
-                }
-
-                if (points.size == competingPlayers) {
-                    Bukkit.broadcastMessage("game ended as all players finished")
-                    GameEngine.stop()
-                }
-            }
-        }
     }
 
     override fun start() {
         startTime = System.currentTimeMillis()
 
-        STARTING_WALL.getBlocks(ChristmasEvent.WORLD).forEach {
-            it.type = Material.AIR
-        }
+        STARTING_WALL.getBlocks(ChristmasEvent.WORLD).applyForEach { type = Material.AIR }
 
-        var timer = 60
-        tasks += repeat(1, TimeUnit.SECONDS) {
-            if (timer == 0) {
+        var timer = 60.0
+        tasks += repeat(5) {
+            Bukkit.getOnlinePlayers().applyForEach { sendActionBar(Component.text("$timer seconds")) }
+
+            if (timer == 0.0) {
                 Bukkit.broadcastMessage("game ended as end timer reached")
                 GameEngine.stop()
                 return@repeat
             }
 
-            Bukkit.getOnlinePlayers().applyForEach { sendActionBar(Component.text("$timer seconds")) }
-            timer--
+            Bukkit.getOnlinePlayers().forEach { player ->
+                if (FINISHING_AREA.contains(player.location) && !points.containsKey(player.uniqueId)) {
+                    points[player.uniqueId] = -1
+                    player.sendMessage("you finished!!! in ${System.currentTimeMillis() - startTime!!}")
+
+                    // MAYBE REPLACE THE FINISH LINE WITH CLEINT SDIE BARRIERS SO CANT GO BACK?
+                }
+            }
+
+            if (points.size == competingPlayers) {
+                Bukkit.broadcastMessage("game ended as all players finished")
+                GameEngine.stop()
+            }
+
+            timer -= 0.25
         }
     }
 
@@ -107,12 +95,10 @@ class SledRacingGame : Game() {
         boats.values.forEach {
             it.remove()
         }
-
-
     }
 
     override fun onPlayerJoin(player: Player) {
-
+        // TP in as a spectator
     }
 
     override fun onPlayerQuit(player: Player) {
@@ -120,9 +106,6 @@ class SledRacingGame : Game() {
             it.remove()
             boats.remove(player)
         }
-
-
-
     }
 
 }
