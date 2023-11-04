f879f2853afd8df9614df8fd2e258b55cff6c7ad
Stephen <138153996+stephendotgg@users.noreply.github.com>
2023-11-04 20:56:04 +0000

    DebugCommand.kt

--- src/main/kotlin/com/learnspigot/event/debug/DebugCommand.kt
+++ src/main/kotlin/com/learnspigot/event/debug/DebugCommand.kt
@@ -1,6 +1,7 @@
 package com.learnspigot.event.debug
 
 import org.bukkit.Bukkit
+import org.bukkit.WorldCreator
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import revxrsal.commands.annotation.Command
@@ -14,6 +15,7 @@ object DebugCommand {
     // temp, no error checking
     @Subcommand("tp")
     fun tp(sender: CommandSender, world: String) {
+        Bukkit.createWorld(WorldCreator("world"))
         (sender as Player).teleport(Bukkit.getWorld(world)!!.spawnLocation)
     }
 
