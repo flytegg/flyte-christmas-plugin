d795ef87f17a9222b830ce92ebdc4eaaa3350674
Stephen <138153996+stephendotgg@users.noreply.github.com>
2023-11-05 02:27:09 +0000

    Game fix

--- src/main/kotlin/com/learnspigot/event/listener/ConnectionListener.kt
+++ src/main/kotlin/com/learnspigot/event/listener/ConnectionListener.kt
@@ -4,8 +4,8 @@ import com.learnspigot.event.ChristmasEvent.Companion.LOBBY_SPAWN
 import com.learnspigot.event.engine.GameEngine
 import com.learnspigot.event.util.npc.NPC
 import com.learnspigot.event.visual.TablistManager
-import gg.flyte.twilight.event.custom.PlayerMainHandInteractEvent
-import gg.flyte.twilight.event.custom.PlayerOffHandInteractEvent
+import gg.flyte.twilight.event.custom.interact.PlayerMainHandInteractEvent
+import gg.flyte.twilight.event.custom.interact.PlayerOffHandInteractEvent
 import gg.flyte.twilight.event.event
 import gg.flyte.twilight.scheduler.delay
 import org.bukkit.Bukkit
